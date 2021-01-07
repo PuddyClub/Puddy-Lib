02e9ee86bdf9b52959c9e9eeae12230d29cfb091
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-01-06 23:17:57 -0300

    super string filter

--- get/super_string_filter.js
+++ get/super_string_filter.js
@@ -32,21 +32,21 @@ module.exports = function (list, validator_list) {
                 tiny_validator.starts = {};
                 tiny_validator.starts.enabled = (typeof validator_list[item2].starts === "string");
                 if (tiny_validator.starts.enabled) {
-                    tiny_validator.starts.result = list[item].startsWith(validator_list[item2].starts);
+                    tiny_validator.starts.result = (list[item].startsWith(validator_list[item2].starts));
                 };
 
                 // Ends With
                 tiny_validator.ends = {};
                 tiny_validator.ends.enabled = (typeof validator_list[item2].ends === "string");
                 if (tiny_validator.ends.enabled) {
-                    tiny_validator.ends.result = list[item].endsWith(validator_list[item2].ends);
+                    tiny_validator.ends.result = (list[item].endsWith(validator_list[item2].ends));
                 };
 
                 // RegExp
                 tiny_validator.regexp = {};
                 tiny_validator.regexp.enabled = objType(validator_list[item2].regexp, 'regexp');
                 if (tiny_validator.regexp.enabled) {
-                    tiny_validator.regexp.result = list[item].match(validator_list[item2].regexp);
+                    tiny_validator.regexp.result = (list[item].match(validator_list[item2].regexp));
                 };
 
                 // Check Validator
