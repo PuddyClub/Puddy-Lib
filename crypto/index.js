74bca4a6ccbe2ee0cffbe9df3abd50271fba673c
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-01-22 23:47:35 -0300

    credits

--- crypto/index.js
+++ crypto/index.js
@@ -1,3 +1,5 @@
+// vlucas/encryption.js
+// https://gist.github.com/vlucas/2bd40f62d20c1d49237a109d491974eb
 module.exports = {
     default: require('./default.json'),
     encrypt: require('./encrypt'),
