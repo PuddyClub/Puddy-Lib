c16a6fae22beb2f7b43905937760d572577e039e
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-01-26 11:29:46 -0300

    level system update

--- libs/leveling.js
+++ libs/leveling.js
@@ -1,36 +1,80 @@
 class levelup {
 
+    // Constructor
     constructor(giveExp, expLevel) {
-
         this.giveExp = giveExp;
         this.expLevel = expLevel;
+    }
+
+    // Get Total Exp
+    getTotalExp(user) {
+
+        // Base
+        let totalexp = 0;
+
+        // For
+        for (let p = 0; p < user.level; p++) {
+            if (p !== 0) {
+                const numberinset = this.expLevel * p;
+                totalexp = totalexp + numberinset;
+            }
+        }
+
+        // Insert Result
+        totalexp = totalexp + user.exp;
+        
+        // Complete
+        return totalexp;
 
     }
 
-    progress(user){
+    // Exp Generator
+    expGenerator(extraExp = 0) {
+        return Number(Math.floor(Math.random() * ((this.giveExp - 1) + 1) + 1)) + extraExp;
+    }
+
+    // Progress
+    progress(user) {
         return this.expLevel * user.level;
     }
 
+    // Give
     give(user, extraExp = 0) {
 
-        user.exp = user.exp + Number(Math.floor(Math.random() * ((this.giveExp - 1) + 1) + 1)) + extraExp;
+        // Get the new user exp
+        user.exp = user.exp + this.expGenerator(extraExp);
+        
+        // Add Level
         if (user.exp > this.expLevel * user.level) {
 
             user.level++;
             user.exp = 0;
-        
+
         }
+
+        // Get Total Exp
+        user.totalExp = this.getTotalExp(user);
+
+        return user;
+
+    }
+
+    // Remove
+    remove(user, extraExp = 0) {
+
+        // Get the new user exp
+        user.exp = user.exp - this.expGenerator(extraExp);
         
-        // Get Exp
-        var totalexp = 0;
-        for (var p = 0; p < user.level; p++) {
-            if (p !== 0) {
-                var numberinset = this.expLevel * p;
-                totalexp = totalexp + numberinset;
-            }
+        // Remove Level
+        if (user.exp < 1) {
+
+            user.level--;
+            user.exp = this.expLevel * user.level;
+
         }
-        totalexp = totalexp + user.exp;
-        user.totalExp = totalexp;
+
+        // Get Total Exp
+        user.totalExp = this.getTotalExp(user);
 
         return user;
 
