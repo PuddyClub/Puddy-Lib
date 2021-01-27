c2db89aa1960c621c96db5f4bc5fb93373dfdf63
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-01-27 20:41:15 -0300

    error fixed

--- http/errorsCallback.js
+++ http/errorsCallback.js
@@ -1,21 +1,15 @@
-module.exports = function (callback) {
+module.exports = function (callback, skipNotFound = false) {
     return function (err, req, res, next) {
 
         // treat as 404
-        if (err.message
+        if (!skipNotFound && err.message
             && (~err.message.indexOf('not found')
                 || (~err.message.indexOf('Cast to ObjectId failed')))) {
             return next()
         }
 
-        //if request accept html request
-        if (req.accepts('html')) {
-            callback(req, res, next, { type: 'html', code: err.status, path: req.url, originalUrl: req.originalUrl, err: err });
-        } else {
-            callback(req, res, next, { code: err.status, path: req.url, originalUrl: req.originalUrl, err: err });
-        }
-
         // Complete
+        callback(req, res, next, { code: err.status, path: req.url, originalUrl: req.originalUrl, err: err });
         return;
 
     };
