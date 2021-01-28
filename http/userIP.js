4d21d5ab38b0aec6ac39928c86c5cabc6cb980ae
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-01-28 18:57:19 -0300

    ip var name

--- http/userIP.js
+++ http/userIP.js
@@ -2,28 +2,28 @@
 module.exports = function (req, options = {}) {
 
     // Prepare IP Value
-    let ip = { result: null, type: null };
+    let ip = { value: null, type: null };
 
     // Detect Headers
     if (req.headers) {
 
         // Exist Client IP
         if (options.isFirebase && req.headers['fastly-client-ip']) {
-            ip.result = req.headers['fastly-client-ip'];
+            ip.value = req.headers['fastly-client-ip'];
             ip.type = 'fastly-client-ip';
         }
 
         // Forwarded
         else if (req.headers['x-forwarded-for']) {
-            ip.result = req.headers['x-forwarded-for'];
+            ip.value = req.headers['x-forwarded-for'];
             ip.type = 'x-forwarded-for';
         }
 
     }
 
     // Connection
-    if (!ip.result && req.connection && req.connection.remoteAddress) {
-        ip.result = req.connection.remoteAddress;
+    if (!ip.value && req.connection && req.connection.remoteAddress) {
+        ip.value = req.connection.remoteAddress;
         ip.type = 'connection.remoteAddress';
     }
 
