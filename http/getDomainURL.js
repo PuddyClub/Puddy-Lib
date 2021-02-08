ab1f8b7babd27bbaf2aef3777b69e07ad129c5a7
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-02-08 13:11:10 -0300

    get domain update

--- http/getDomainURL.js
+++ http/getDomainURL.js
@@ -1,13 +1,26 @@
 module.exports = function (domain, httpResult = 'https') {
 
+    // Domain Selected
+    let domainSelected = null;
+
     // String
     if (typeof domain === "string") {
+        domainSelected = domain;
+    }
+
+    // Nope
+    else {
+        domainSelected = require('./check_domain').get(domain);
+    }
+
+    // Domain
+    if (typeof domainSelected === "string") {
 
         // Normal Domain
-        if (!domain.startsWith('localhost:')) { return `${httpResult}://${domain}`; }
+        if (!domainSelected.startsWith('localhost:')) { return `${httpResult}://${domainSelected}`; }
 
         // Localhost
-        else { return `http://${domain}`; }
+        else { return `http://${domainSelected}`; }
 
     }
 
