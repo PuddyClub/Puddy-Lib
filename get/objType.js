516adc0b5030c5e24dcde3f6aa03593f71b3a929
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-02-10 23:57:04 -0300

    objType update

--- get/objType.js
+++ get/objType.js
@@ -1,7 +1,31 @@
 module.exports = function (obj, type) {
-    if (Object.prototype.toString.call(obj).toLowerCase() === `[object ${type}]`) {
-        return true;
-    } else {
-        return false;
+
+    // Is Defined
+    if (typeof obj !== "undefined") {
+
+        // Check Obj Type
+        if (typeof type === "string") {
+            if (Object.prototype.toString.call(obj).toLowerCase() === `[object ${type}]`) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        // Get Obj Type
+        else {
+
+            // Result
+            const result = Object.prototype.toString.call(obj).toLowerCase();
+            
+            // Send Result
+            return result.substring(8, result.length - 1);
+
+        }
+
     }
+
+    // Nope
+    else { return null; }
+
 };
