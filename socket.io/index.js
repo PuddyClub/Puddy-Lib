1dd46cc7700342b0131b6fd1968e38f9913ed932
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-03-30 19:56:10 -0300

    socket.io index

--- socket.io/index.js
+++ socket.io/index.js
@@ -1,3 +1,5 @@
 module.exports = {
-    'antiFlood': require('./antiFlood')
+    'antiFlood': require('./antiFlood'),
+    'cookie-session': require('./cookie-session'),
+    'discord': require('./discord')
 };
