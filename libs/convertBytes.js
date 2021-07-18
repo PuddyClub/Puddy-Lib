f64b8cb98e156d0b011b0b1565235a5151718256
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-07-18 09:21:00 -0300

    preparing convert bytes

---
+++ libs/convertBytes.js
@@ -0,0 +1,12 @@
+const convertList = {
+    kb: 1024,
+    mb: 1048576,
+    gb: 1073741824,
+    tb: 1099511627776,
+    pb: 1125899906842624,
+    eb: 1152921504606847000
+};
+
+module.exports = function (bytes) {
+    return ;
+};
